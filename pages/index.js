a2ad519d2e1fefaad87b8505476f3b758407d03c
toho36 <toho0036@gmail.com>
2023-08-01 17:50:56 +0200

    256. rendering dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import MeetupList from '../components/meetups/MeetupList';
 
-function mainPage() {
-  return <div>mainPage</div>;
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'first meetup',
+    image:
+      'https://www.travelandleisure.com/thmb/9xr8CFGR14sLvR4IhLwKV64fEV0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-Eiffel-Tower-BESTFRANCE0323-dada0673f8764c099b68d01695ef8057.jpg',
+    address: 'Paris adress 5, 12345, france',
+    description: 'First meetup',
+  },
+  {
+    id: 'm2',
+    title: 'second meetup',
+    image:
+      'https://www.travelandleisure.com/thmb/9xr8CFGR14sLvR4IhLwKV64fEV0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-Eiffel-Tower-BESTFRANCE0323-dada0673f8764c099b68d01695ef8057.jpg',
+    address: 'Potato adress 5, 12345, france',
+    description: 'this is a second meetup',
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
-export default mainPage;
+export default HomePage;
