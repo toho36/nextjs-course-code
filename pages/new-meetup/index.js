efa3918f4119d68213644266f7cc466212639db8
toho36 <toho0036@gmail.com>
2023-08-02 10:48:00 +0200

    257. Adding form
    from component

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeeetUp() {
-  return <div>NewMeeetUp</div>;
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
 
 export default NewMeeetUp;
