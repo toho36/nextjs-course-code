e8feb41b5878e8599123740bd992119f911c9482
toho36 <toho0036@gmail.com>
2023-08-02 11:50:13 +0200

    260 Custom components with styling

--- pages/[meetUpId]/index.js
+++ pages/[meetUpId]/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-
-function MeetUp() {
-  return <div>MeetUp</div>;
+import React, { Fragment } from 'react';
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://www.travelandleisure.com/thmb/9xr8CFGR14sLvR4IhLwKV64fEV0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-Eiffel-Tower-BESTFRANCE0323-dada0673f8764c099b68d01695ef8057.jpg"
+      title="first meetup"
+      address="Some street 5"
+      description="this is a first meetup"
+    />
+  );
 }
 
-export default MeetUp;
+export default MeetupDetails;
