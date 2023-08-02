f011f6130c6d73c56e21f97c61cbfe84156a5426
toho36 <toho0036@gmail.com>
2023-08-02 10:59:24 +0200

    258. changing layout to _app.js
    will add to every page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import MeetupList from '../components/meetups/MeetupList';
-import Layout from '../components/layout/Layout';
 const DUMMY_MEETUPS = [
   {
     id: 'm1',
@@ -21,11 +20,7 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage() {
-  return (
-    <Layout>
-      <MeetupList meetups={DUMMY_MEETUPS} />{' '}
-    </Layout>
-  );
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
